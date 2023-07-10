126a58185c50126596820a7d8aedc9b47dca89a0
DSlayer <93089045+sharon123454@users.noreply.github.com>
2023-07-10 03:04:48 +0300

    dowteen

---
+++ Assets/_Scripts/B2Manager.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class B2Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
